370ff80f758b291300759fade57f8c0e76c8a5f2
bpearce@angelo.edu <bpearce@angelo.edu>
2018-10-06 20:31:49 -0500

    setup discord config

---
+++ NodeBot/Config.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeBot
+{
+    class Config
+    {
+        private const string configFolder = "Resources";
+        private const string configFile = "config.json";
+        public static BotConfig bot;
+        // constructor
+        static Config()
+        {
+            if (!Directory.Exists(configFolder)) Directory.CreateDirectory(configFolder);
+
+            if (!File.Exists(configFolder + "/" + configFile))
+            {
+                // if the config does not exist, create and setup the configuration file
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(configFolder + "/" + configFile, json);
+            }
+            else
+            {
+                // if it does exist, set json config values to bot
+                string json = File.ReadAllText(configFolder + "/" + configFile);
+                bot = JsonConvert.DeserializeObject<BotConfig>(json);
+
+            }
+        }
+    }
+
+    public struct BotConfig
+    {
+        public string token;
+        public string cmdPrefix;
+    }
+}
--- NodeBot/Program.cs
+++ NodeBot/Program.cs
@@ -10,7 +10,8 @@ namespace NodeBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Utilities.GetAlert("TEST"));
+            Console.WriteLine("Test:" + Config.bot.token);
+            Console.ReadLine();
         }
     }
 }
