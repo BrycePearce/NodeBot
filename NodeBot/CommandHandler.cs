a24a2da20eed3a65a332d9e915390bdd162a30e2
bpearce@angelo.edu <bpearce@angelo.edu>
2018-10-06 22:31:15 -0500

    error logging, and boilerplate to log in and run the bot

---
+++ NodeBot/CommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace NodeBot
+{
+    class CommandHandler
+    {
+        DiscordSocketClient _client;
+        CommandService _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client; // discord client
+            _service = new CommandService(); // discord service
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly()); // adds modules asynchronously 
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage; // convert? to SocketUserMessage. Because that's what SocketCommandContext needs
+
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+            int messagePosition = 0;
+            if (msg.HasStringPrefix(Config.bot.cmdPrefix, ref messagePosition) || msg.HasMentionPrefix(_client.CurrentUser, ref messagePosition)) // if message has our prefix, at position 0. Or if the bot (current user) has been mentioned e.g. @NodeBot at position 0
+            {
+                // execute the command
+                var result = await _service.ExecuteAsync(context, messagePosition);
+
+                // if something went wrong, write out the error
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.Write(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
--- NodeBot/Program.cs
+++ NodeBot/Program.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
 
 namespace NodeBot
 {
     class Program
     {
+        DiscordSocketClient _client;
+        CommandHandler _handler;
         static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult(); // tell main to start asynchronously
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Test:" + Config.bot.token);
-            Console.ReadLine();
+            // make sure we have a token
+            if (Config.bot.token == "" || Config.bot.token == null) return;
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose // verbose logging for debugging
+            });
+            // subscribe to a log event
+            _client.Log += Log;
+   
+            // Log in and start the bot
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+
+            _handler = new CommandHandler();
+            await _handler.InitializeAsync(_client);
+            await Task.Delay(-1); // wait until the operation ends
+
+        }
+
+        private async Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
         }
     }
 }
