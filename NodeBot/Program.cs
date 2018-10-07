48ebe6ef8cfc9aaab5c8829169f37452b9b3c370
bpearce@angelo.edu <bpearce@angelo.edu>
2018-10-06 19:20:48 -0500

    load in json, basic structure setup

--- NodeBot/Program.cs
+++ NodeBot/Program.cs
@@ -10,6 +10,7 @@ namespace NodeBot
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Utilities.GetAlert("TEST"));
         }
     }
 }
--- NodeBot/Utilities.cs
+++ NodeBot/Utilities.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace NodeBot
 {
     class Utilities
     {
+        // Dictionary in C# is a collection of Keys and Values, where key is like word and value is like definition.
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
     }
 }
