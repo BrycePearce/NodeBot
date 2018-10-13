1fdaa98398f4f2242fd2faa3151e9d8231c70c6e
bpearce@angelo.edu <bpearce@angelo.edu>
2018-10-12 22:27:23 -0500

    google geolocation and darksky api integration for weather functionality

---
+++ NodeBot/src/Models/DarkSkyModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeBot.src.Models
+{
+    public class Currently
+    {
+        public int time { get; set; }
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public int nearestStormDistance { get; set; }
+        public int nearestStormBearing { get; set; }
+        public int precipIntensity { get; set; }
+        public int precipProbability { get; set; }
+        public double temperature { get; set; }
+        public double apparentTemperature { get; set; }
+        public double dewPoint { get; set; }
+        public double humidity { get; set; }
+        public double pressure { get; set; }
+        public double windSpeed { get; set; }
+        public double windGust { get; set; }
+        public int windBearing { get; set; }
+        public double cloudCover { get; set; }
+        public int uvIndex { get; set; }
+        public int visibility { get; set; }
+        public double ozone { get; set; }
+    }
+
+    public class Datum
+    {
+        public int time { get; set; }
+        public int precipIntensity { get; set; }
+        public int precipProbability { get; set; }
+    }
+
+    public class Minutely
+    {
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public List<Datum> data { get; set; }
+    }
+
+    public class Datum2
+    {
+        public int time { get; set; }
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public double precipIntensity { get; set; }
+        public double precipProbability { get; set; }
+        public double temperature { get; set; }
+        public double apparentTemperature { get; set; }
+        public double dewPoint { get; set; }
+        public double humidity { get; set; }
+        public double pressure { get; set; }
+        public double windSpeed { get; set; }
+        public double windGust { get; set; }
+        public int windBearing { get; set; }
+        public double cloudCover { get; set; }
+        public int uvIndex { get; set; }
+        public int visibility { get; set; }
+        public double ozone { get; set; }
+        public string precipType { get; set; }
+    }
+
+    public class Hourly
+    {
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public List<Datum2> data { get; set; }
+    }
+
+    public class Datum3
+    {
+        public int time { get; set; }
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public int sunriseTime { get; set; }
+        public int sunsetTime { get; set; }
+        public double moonPhase { get; set; }
+        public double precipIntensity { get; set; }
+        public double precipIntensityMax { get; set; }
+        public int precipIntensityMaxTime { get; set; }
+        public double precipProbability { get; set; }
+        public string precipType { get; set; }
+        public double temperatureHigh { get; set; }
+        public int temperatureHighTime { get; set; }
+        public double temperatureLow { get; set; }
+        public int temperatureLowTime { get; set; }
+        public double apparentTemperatureHigh { get; set; }
+        public int apparentTemperatureHighTime { get; set; }
+        public double apparentTemperatureLow { get; set; }
+        public int apparentTemperatureLowTime { get; set; }
+        public double dewPoint { get; set; }
+        public double humidity { get; set; }
+        public double pressure { get; set; }
+        public double windSpeed { get; set; }
+        public double windGust { get; set; }
+        public int windGustTime { get; set; }
+        public int windBearing { get; set; }
+        public double cloudCover { get; set; }
+        public int uvIndex { get; set; }
+        public int uvIndexTime { get; set; }
+        public int visibility { get; set; }
+        public double ozone { get; set; }
+        public double temperatureMin { get; set; }
+        public int temperatureMinTime { get; set; }
+        public double temperatureMax { get; set; }
+        public int temperatureMaxTime { get; set; }
+        public double apparentTemperatureMin { get; set; }
+        public int apparentTemperatureMinTime { get; set; }
+        public double apparentTemperatureMax { get; set; }
+        public int apparentTemperatureMaxTime { get; set; }
+    }
+
+    public class Daily
+    {
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public List<Datum3> data { get; set; }
+    }
+
+
+    public class DarkSkyResponse
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string timezone { get; set; }
+        public Currently currently { get; set; }
+        public Minutely minutely { get; set; }
+        public Hourly hourly { get; set; }
+        public Daily daily { get; set; }
+        public int offset { get; set; }
+    }
+}
---
+++ NodeBot/src/Modules/Weather.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json;
+using NodeBot.src.Models;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NodeBot.src.Modules
+{
+    public class Weather : ModuleBase<SocketCommandContext>
+    {
+        private static readonly HttpClient client = new HttpClient(); // I think adding this here might be bad practice. Not sure. todo: findout
+        [Command("weather")]
+        public async Task WeatherInformation([Remainder] string message) // [Remainder] gives us the whole message as one long string
+        {
+
+            // Call google to geocode given address, store lat lng for darksky api
+            string geocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + message + "&key=" + Config.weatherTokens.googleGeoToken; // todo: move key to json
+
+            var geoResponse = await client.GetAsync(geocodeUrl);
+            var geoResponseString = await geoResponse.Content.ReadAsStringAsync();
+
+            GeocodeResponse geoFormattedResponse = JsonConvert.DeserializeObject<GeocodeResponse>(geoResponseString);
+
+            var result = geoFormattedResponse.results;
+            double lat = result.FirstOrDefault(x => x.geometry.location != null)?.geometry.location.lat ?? 0;
+            double lng = result.FirstOrDefault(x => x.geometry.location != null)?.geometry.location.lng ?? 0;
+
+            // Call DarkSky api with lat lng
+            string darkSkyRequestUrl = "https://api.darksky.net/forecast/" + Config.weatherTokens.darkSkyToken + "/" + lat + "," + lng;
+
+            var darkResponse = await client.GetAsync(darkSkyRequestUrl);
+            var darkResponseString = await darkResponse.Content.ReadAsStringAsync();
+
+            //DarkSkyResponse darkSkyResponse = JsonConvert.DeserializeObject<DarkSkyResponse>(darkResponseString); // not working, can't figure out why. todo: fix
+            dynamic deserializedDarkResponse = JsonConvert.DeserializeObject(darkResponseString);
+            await Context.Channel.SendMessageAsync("The temperature in " + message + " is currently: " + deserializedDarkResponse.currently.temperature + "F");
+        }
+    }
+}
