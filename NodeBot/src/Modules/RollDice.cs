0900c91a49a67075443a7de7cd4d274e9a04407e
bpearce@angelo.edu <bpearce@angelo.edu>
2019-11-03 17:01:09 -0600

    updated dice roll

---
+++ NodeBot/src/Modules/RollDice.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Commands;
+using RollGen;
+using RollGen.IoC;
+using System.Threading.Tasks;
+
+namespace NodeBot.src.Modules
+{
+    public class RollDice : ModuleBase<SocketCommandContext>
+    {
+        [Command("roll")]
+        [Alias("r", "d")]
+        public async Task RollD20([Remainder] string message) // [Remainder] gives us the message after the command try = "" again
+        {
+            var roll = RollGen(message);
+            await ReplyAsync($":: Total {roll.AsSum()} / {roll.AsPotentialMaximum()} [{roll.AsPotentialAverage()}%] :: Results [{string.Join(", ", roll.AsIndividualRolls())}] ::");
+        }
+
+        private PartialRoll RollGen(string diceRoll)
+        {
+            var myDice = DiceFactory.Create();
+            return myDice.Roll(diceRoll);
+        }
+    }
+}
